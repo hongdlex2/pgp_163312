b091eccf63b375c1eb2af463ed9d3f28eef05441
tkskd <tkskd@192.168.56.1>
2019-12-06 22:58:47 +0900

    1206

--- CeLcokerManager/src/clm/Locker.java
+++ CeLcokerManager/src/clm/Locker.java
@@ -1,5 +1,5 @@
 package clm;
-//
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
--- CeLcokerManager/src/clm/MainTest.java
+++ CeLcokerManager/src/clm/MainTest.java
@@ -1,5 +1,5 @@
 package clm;
-//
+
 public class MainTest {
 
 	public static void main(String[] args) {
